ac82cbfdee9ee63a33908c45581ab479eb261d16
lystv <117786160+lystv@users.noreply.github.com>
2026-09-13 15:14:15 +0800

    适配车机

--- android/app/src/main/java/cn/toside/music/mobile/MainActivity.java
+++ android/app/src/main/java/cn/toside/music/mobile/MainActivity.java
@@ -1,13 +1,39 @@
 package cn.toside.music.mobile;
 
-import com.reactnativenavigation.NavigationActivity;
+import android.view.KeyEvent;
+import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
-public class MainActivity extends NavigationActivity {
+public class MainActivity extends ReactActivity {
 
+    @Override
+    protected String getMainComponentName() {
+        return "lx-music";
+    }
 
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(
+            this,
+            getMainComponentName(),
+            DefaultNewArchitectureEntryPoint.getFabricEnabled()
+        );
+    }
 
-
+    // 核心：拦截车机方向盘媒体按键及电视遥控器按键，防止无响应
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MEDIA_NEXT:
+            case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+            case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+            case KeyEvent.KEYCODE_MEDIA_PLAY:
+            case KeyEvent.KEYCODE_HEADSETHOOK:
+                return super.onKeyDown(keyCode, event);
+            default:
+                return super.onKeyDown(keyCode, event);
+        }
+    }
 }
